b4eb6feba4640c19d40f2c6622b201800deac759
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-01 02:33:59 +0530

    heade and home section

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../Components/Layouts/Layout';
+import Section1 from './Section1';
+import '../../styles/HomeStyle.css';
+const Home = () => {
+  return (
+    <Layout>
+      {/* Home Section Hero Banner */}
+      <Section1 />
+    </Layout>
+  );
+};
+
+export default Home;
