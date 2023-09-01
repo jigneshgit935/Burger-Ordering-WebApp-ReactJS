2134d9eab8ad46680fd019a55d045ff7ebc87849
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-01 16:11:42 +0530

    about section commited

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Layout from '../../Components/Layouts/Layout';
 import Section1 from './Section1';
 import '../../styles/HomeStyle.css';
+import Section2 from './Section2';
 const Home = () => {
   return (
     <Layout>
       {/* Home Section Hero Banner */}
       <Section1 />
+
+      {/* Home Section About */}
+      <Section2 />
     </Layout>
   );
 };
