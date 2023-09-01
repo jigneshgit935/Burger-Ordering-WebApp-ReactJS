33b81109ee682e374d4e52eb026779cd5ed2faf5
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-02 03:32:20 +0530

    Contact done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,7 @@ import Section3 from './Section3';
 import Section4 from './Section4';
 import Section5 from './Section5';
 import Section6 from './Section6';
+import Section7 from './Section7';
 const Home = () => {
   return (
     <Layout>
@@ -27,6 +28,9 @@ const Home = () => {
 
       {/* Blog */}
       <Section6 />
+
+      {/* Cotact */}
+      <Section7 />
     </Layout>
   );
 };
