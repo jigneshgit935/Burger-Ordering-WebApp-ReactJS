d83736c87b466a9d74143b29457a1c99f6213620
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-02 04:17:57 +0530

    ready-to-deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,94 +1,124 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  //Scrolltop function
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    windowScroll > heightToHidden ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenToScroll);
+  });
+
   return (
-    <footer>
-      <Container>
-        <Row>
-          <Col sm={6} lg={3} className="mb-4 mb-lg-0">
-            <div className="text-center">
-              <h5>Location</h5>
-              <p>5505 Waterford District</p>
-              <p>Dr, Miami, FL 33126</p>
-              <p>United States</p>
-            </div>
-          </Col>
-          <Col sm={6} lg={3} className="mb-4 mb-lg-0">
-            <div className="text-center">
-              <h5>Working Hours</h5>
-              <p>Mon-Fri: 9:00AM - 10:00PM</p>
-              <p>Saturday: 10:00AM - 8:30PM</p>
-              <p>Sunday: 12:00PM - 5:00PM</p>
-            </div>
-          </Col>
-          <Col sm={6} lg={3} className="mb-4 mb-lg-0">
-            <div className="text-center">
-              <h5>Order Now</h5>
-              <p>Quaerat neque purus ipsum</p>
-              <p>
-                <Link to="tel:9998887777" className="calling">
-                  9998887777
-                </Link>
-              </p>
-            </div>
-          </Col>
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Location</h5>
+                <p>5505 Waterford District</p>
+                <p>Dr, Miami, FL 33126</p>
+                <p>United States</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Working Hours</h5>
+                <p>Mon-Fri: 9:00AM - 10:00PM</p>
+                <p>Saturday: 10:00AM - 8:30PM</p>
+                <p>Sunday: 12:00PM - 5:00PM</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Order Now</h5>
+                <p>Quaerat neque purus ipsum</p>
+                <p>
+                  <Link to="tel:9998887777" className="calling">
+                    9998887777
+                  </Link>
+                </p>
+              </div>
+            </Col>
 
-          <Col sm={6} lg={3} className="mb-4 mb-lg-0">
-            <div className="text-center">
-              <h5>Follow Us</h5>
-              <p>Quaerat neque purus ipsum</p>
-              <ul className="list-unstyled text-center mt-2">
-                <li>
-                  <Link to="/">
-                    <i className="bi bi-facebook"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/">
-                    <i className="bi bi-twitter"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/">
-                    <i className="bi bi-instagram"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/">
-                    <i className="bi bi-youtube"></i>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Follow Us</h5>
+                <p>Quaerat neque purus ipsum</p>
+                <ul className="list-unstyled text-center mt-2">
+                  <li>
+                    <Link to="/">
+                      <i className="bi bi-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i className="bi bi-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i className="bi bi-instagram"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i className="bi bi-youtube"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
 
-        <Row className="copy_right">
-          <Col>
-            <div>
-              <ul className="list-unstyled text-center mb-0">
-                <li>
-                  <Link to="/">
-                    © 2023 <span>JIGNESHGIT935</span>. All Rights Reserved
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/">About Us</Link>
-                </li>
-                <li>
-                  <Link to="/">Terms Of Use</Link>
-                </li>
-                <li>
-                  <Link to="/">Privacy Policy</Link>
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+          <Row className="copy_right">
+            <Col>
+              <div>
+                <ul className="list-unstyled text-center mb-0">
+                  <li>
+                    <Link to="/">
+                      © 2023 <span>JIGNESHGIT935</span>. All Rights Reserved
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Terms Of Use</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Privacy Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+      {/* Scrolltop */}
+      {isVisible && (
+        <div className="scroll_top" onClick={scrollTop}>
+          <i className="bi bi-arrow-up"></i>
+        </div>
+      )}
+    </>
   );
 };
 
