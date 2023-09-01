13ffe773ab3ef18463666fc9ba7b9dab2ad05552
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-02 02:48:55 +0530

    shop promotion commited

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import '../../styles/HomeStyle.css';
 import Section2 from './Section2';
 import Section3 from './Section3';
 import Section4 from './Section4';
+import Section5 from './Section5';
 const Home = () => {
   return (
     <Layout>
@@ -18,8 +19,10 @@ const Home = () => {
       <Section3 />
 
       {/* Home Section Promotion */}
-
       <Section4 />
+
+      {/* Shop Section */}
+      <Section5 />
     </Layout>
   );
 };
